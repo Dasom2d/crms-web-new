6cb999490d3305b6ef8decd0bd44c5792f37053a
somi.da <somi.da@kakaopaycorp.com>
2021-11-11 01:48:14 +0900

    [add] header와 css

---
+++ src/test/java/com/kakaopay/crms/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaopay.crms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
